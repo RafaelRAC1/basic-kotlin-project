42b0440479a4b1af8eda82959bb42f3de4e28596
Rafael Reis dos Anjos Corrêa <91158868+RafaelRAC1@users.noreply.github.com>
2023-05-17 23:58:00 -0300

    classes development

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,41 @@
+data class User(val name: String, val age: Int){
+    private val attendedCourses = mutableListOf<Course>()
+
+    fun registerIntoCourse(course: Course){
+        attendedCourses.add(course)
+        course.getRegisteredUsers().add(this)
+    }
+
+    fun getAttendedCourses(): List<Course>{
+        return attendedCourses
+    }
+}
+
+enum class Level { EASY, INTERMEDIATE, ADVANCED}
+
+data class Content(val name: String, val duration: Int)
+
+data class Course(val name: String, val difficulty: Level) {
+    private val registeredUsers = mutableListOf<User>()
+    private val registeredContents = mutableListOf<Content>()
+
+    fun registerContent(content: Content) {
+        registeredContents.add(content)
+    }
+
+    fun registerMultipleContent(vararg content: Content) {
+        registeredContents.addAll(content)
+    }
+
+    fun getRegisteredContents(): List<Content>{
+        return registeredContents
+    }
+
+    fun getRegisteredUsers(): MutableList<User>{
+        return registeredUsers
+    }
+}
+
 fun main() {
     println("Hello World!")
 }
