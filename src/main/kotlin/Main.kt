98bb4c59a217b7d27404cf34f6977e2d18b67256
Rafael Reis dos Anjos Corrêa <91158868+RafaelRAC1@users.noreply.github.com>
2023-05-17 23:58:28 -0300

    updating main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -36,6 +36,56 @@ data class Course(val name: String, val difficulty: Level) {
     }
 }
 
-fun main() {
-    println("Hello World!")
+fun main(){
+    //Creating users
+    val userRafael = User("Rafael", 18)
+    val userToya = User("Toya", 3)
+
+    //Creating courses and their contents
+    val javaCourse = Course("Java Basics", Level.EASY)
+    javaCourse.registerContent(Content("Variables", 10))
+
+    val kotlinCourse = Course("Kotlin Basics", Level.EASY)
+    kotlinCourse.registerContent(Content("Data Types", 5))
+
+    //Registering users into courses
+    userRafael.registerIntoCourse(javaCourse)
+    userRafael.registerIntoCourse(kotlinCourse)
+
+    userToya.registerIntoCourse(kotlinCourse)
+
+    //Using a function to insert multiple contents into a course
+    javaCourse.registerMultipleContent(
+        Content("Conditionals", 7),
+        Content("Data Structures", 20)
+    )
+    kotlinCourse.registerMultipleContent(
+        Content("Variables", 5),
+        Content("Data Structures", 20)
+    )
+
+    //Print the info
+    println("-----------Users Info-------------")
+    println("Courses attended by "+userRafael.name+":\n"
+            +userRafael.getAttendedCourses())
+    println()
+    println("Courses attended by "+userToya.name+":\n"
+            +userToya.getAttendedCourses())
+    println("------------------------------------")
+    println()
+
+    println("-----------Courses Info-------------")
+    println("Course: "+javaCourse.name+"\n"
+            +"Registered Users: "
+            +javaCourse.getRegisteredUsers()+"\n"
+            +"Registered Contents: "
+            +javaCourse.getRegisteredContents())
+    println()
+    println("Course: "+kotlinCourse.name+"\n"
+            +"Registered Users: "
+            +kotlinCourse.getRegisteredUsers()+"\n"
+            +"Registered Contents: "
+            +kotlinCourse.getRegisteredContents())
+    println("------------------------------------")
+
 }
